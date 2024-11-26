376e795943c77868a947cf13615d2ac5cad99b29
Crypto-Barroca <btcto154k@gmail.com>
2024-11-26 13:57:22 +0000

    Test Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
